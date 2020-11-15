a479f424df7dfee5138687cd7b48a11ec9c6949a
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-15 00:38:54 -0500

    First codes

---
+++ src/day02_FirstJavaCodes/PrintFamilyMembers.java
@@ -0,0 +1,12 @@
+package day02_FirstJavaCodes;
+
+import java.sql.SQLOutput;
+
+public class PrintFamilyMembers {
+    public static void main(String[] args) {
+        System.out.println("Yalcin");
+        System.out.println("Adem");
+        System.out.println("Volkan");
+        System.out.println("Mukkaddes");
+    }
+}
