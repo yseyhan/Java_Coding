f510d536afac3a44eab6a9b280e0cf599c1cd1b3
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-15 11:55:24 -0500

    First codes

---
+++ src/day04_MyVariablesContinues/BooleanVariables.java
@@ -0,0 +1,26 @@
+package day04_MyVariablesContinues;
+
+public class BooleanVariables {
+
+    public static void main(String[] args) {
+
+
+        boolean r1 = true;
+        boolean r2= false;
+
+        boolean r3= 10>9;
+
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+
+        System.out.println("==============================================");
+
+
+
+
+
+    }
+
+}
