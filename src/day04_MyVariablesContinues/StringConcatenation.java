dbd8c48ffe1b74eb2c618cb5366ec76815c69d05
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-15 13:13:09 -0500

    First codes

---
+++ src/day04_MyVariablesContinues/StringConcatenation.java
@@ -0,0 +1,18 @@
+package day04_MyVariablesContinues;
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+// the address noted for success
+        int houseNumber = 1510;
+        String streetName = "Federalist Way";
+        String cityName ="Arlington";
+        String state = "VA";
+        int zipCode = 20302;
+
+        String fullAddress= houseNumber+" "+streetName+" "+cityName+" "+state+" "+zipCode;
+        System.out.println(fullAddress);
+
+
+
+    }
+}
