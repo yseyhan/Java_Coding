8bc161480aad5d977d0307886098e6ef858c63b0
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-15 13:07:11 -0500

    First codes

---
+++ src/day04_MyVariablesContinues/PrimitiveNumberPractice.java
@@ -0,0 +1,14 @@
+package day04_MyVariablesContinues;
+
+public class PrimitiveNumberPractice {
+    public static void main(String[] args) {
+        byte num1=100;
+        short num2=1000;
+        int num3= 100000;
+        long num4= 1000000;
+        float num5 =98.99f;
+        double num6= 100000000.9d;
+
+
+    }
+}
