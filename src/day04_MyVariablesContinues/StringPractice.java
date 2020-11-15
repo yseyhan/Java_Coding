f43884d01148522e8a4933231cfdfa9c08419e26
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-15 13:02:56 -0500

    First codes

---
+++ src/day04_MyVariablesContinues/StringPractice.java
@@ -0,0 +1,22 @@
+package day04_MyVariablesContinues;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        // firstName, lastName, yourAddress
+        String firstName = "yalcin";
+        String lastName = "seyhan";
+        String emailDomain= "hotmail.com";
+
+        String emailAddress =  firstName+'.'+lastName+'@'+emailDomain;
+        // it is my real email so let me know if you have any questions for Java
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(emailAddress);
+
+
+
+    }
+
+
+}
