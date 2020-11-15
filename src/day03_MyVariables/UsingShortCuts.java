1947483e472bacbaeb20359f83bab013a5713244
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-15 11:17:25 -0500

    First codes

---
+++ src/day03_MyVariables/UsingShortCuts.java
@@ -0,0 +1,30 @@
+package day03_MyVariables;
+
+public class UsingShortCuts {
+    public static void main(String[] args) {
+        System.out.println("Hello Every\n\tMy name is 'Yalcin', my favorite music is \"Believer\", my favorite show is \"Game Of Throne\"");
+
+        System.out.println("My favorite book is \"Java\".");
+
+        System.out.println("================================================");
+        System.out.println("\n\t\t* * * * *\n\t\t* * * * *\n\t\t* * * * *\n\t\t* * * * *\n\t\t* * * * *");
+        // each 5 * will be printed in new line with 2 paragraph spaces
+
+
+
+    }
+}
+/*
+Task:
+    1. create a class called AboutMySelf
+            write a paragraph to tell us about your self
+                Must include:
+                        favorite music
+                        book
+                        TV series
+    *****
+    *****
+    *****
+    *****
+    *****
+ */
