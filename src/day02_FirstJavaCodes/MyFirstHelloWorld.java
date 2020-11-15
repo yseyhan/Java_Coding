f7e49b081494f234cfaf1823a9f19ff245dee3ff
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-15 00:52:36 -0500

    First codes

---
+++ src/day02_FirstJavaCodes/MyFirstHelloWorld.java
@@ -0,0 +1,16 @@
+package day02_FirstJavaCodes;
+
+public class MyFirstHelloWorld {
+    // Hi universe a new programming is coming along
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Universe");
+
+        System.out.println("Nice to code");
+
+        System.out.println("My name is: Yalcin");
+
+
+    }
+}
