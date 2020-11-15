86d2d9885f7045dac1e7e35d649034bad94b04d2
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-15 00:50:11 -0500

    First codes

---
+++ src/day02_FirstJavaCodes/CreatingAReceipt.java
@@ -0,0 +1,20 @@
+package day02_FirstJavaCodes;
+
+public class CreatingAReceipt {
+    // I forgot the ask for the receipt after shopping decided to create new one
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("     MCLEAN COFFEE STORE");
+        System.out.println();
+        System.out.println("2020-11-15  04:12PM");
+        System.out.println();
+        System.out.println("Coffee:        10.870");
+        System.out.println("Price         :$ 2.089");
+        System.out.println();
+        System.out.println("Total         :$ 22.71");
+
+
+    }
+}
