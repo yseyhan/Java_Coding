fdb1d1802b3c696ae07d8923b1e271f88d4596e9
yseyhan <yalcin.seyhan@hotmail.com>
2020-11-16 23:24:35 -0500

    Getting closer to real programming

---
+++ src/ScannerWithStrings/CharAt.java
@@ -0,0 +1,24 @@
+package ScannerWithStrings;
+
+public class CharAt {
+
+    public static void main(String[] args) {
+
+        String schoolName= "Hopkins";
+
+        System.out.println(schoolName.charAt(0));
+        char  second= schoolName.charAt(1);
+        System.out.println(second);
+
+        char last= schoolName.charAt(schoolName.length()-1);
+        System.out.println(last);
+
+
+
+
+
+    }
+
+
+
+}
